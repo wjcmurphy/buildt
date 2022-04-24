bdf3fc13501cf13412e70155ae20549732cd41d5
wjcmurphy <j.murphy@utexas.edu>
2022-04-23 22:26:20 -0500

    Update settings.py
    working on digital ocean build

--- buildt/settings.py
+++ buildt/settings.py
@@ -17,9 +17,10 @@ from re import A
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+STATIC_URL = "/static/"
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-stdvv6z2vfz#e%a5_205j2&f_g5q5lv!16rje_e%^gqkt*pfth'
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-stdvv6z2vfz#e%a5_205j2&f_g5q5lv!16rje_e%^gqkt*pfth
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 LOGIN_REDIRECT_URL = '/accounts/profile'
 
