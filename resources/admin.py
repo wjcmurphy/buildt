13720c9d4ed52c121b0b29470cfe5d9fab3196b0
Craig Kahle <ckahle33@gmail.com>
2022-05-15 23:31:07 -0500

    model updates

--- resources/admin.py
+++ resources/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Resource, Skill, Availability
+from .models import Resource, Skill, Availability, Booking
 
 # Register your models here.
 admin.site.register(Resource)
 admin.site.register(Skill)
 admin.site.register(Availability)
+# admin.site.register(Booking)
--- resources/models.py
+++ resources/models.py
@@ -12,6 +12,7 @@ class Skill(models.Model):
     skill_name = models.CharField(max_length=400)  
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
+
     
     def __str__(self):
         return self.skill_name
@@ -22,7 +23,7 @@ class Resource(models.Model):
     last_name = models.CharField(max_length=400)
     title = models.CharField(max_length=400)
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    skills = models.ManyToManyField(Skill)
+    skills = models.ManyToManyField(Skill, through='ResourceSkill')
     time_zone = models.CharField(max_length=25, default='cst')
     cost_per_hour = models.FloatField(default=10)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
@@ -31,8 +32,8 @@ class Resource(models.Model):
 class Availability(models.Model):
     # ResourceAvailability fields here
     start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    hours_per_day = models.FloatField()
+    duration_months = models.FloatField()
+    hours_per_month = models.FloatField()    
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
     resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
@@ -46,22 +47,9 @@ class Booking(models.Model):
     updated_at = models.DateTimeField(auto_now=True, blank=True)
     resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
 
-# class SkillLevel(models.Model):
-#     # Skill fields here
-#     skill_level = models.CharField(max_length=400)
-#     created_at = models.DateTimeField(auto_now_add=True, blank=True)
-#     updated_at = models.DateTimeField(auto_now=True, blank=True)
-
-# class ResourceSkills(models.Model):
-#     # ResourceSkills fields here
-#     resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
-#     skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
-#     created_at = models.DateTimeField(auto_now_add=True, blank=True)
-#     updated_at = models.DateTimeField(auto_now=True, blank=True)
-
-# class ResourceSkillLevel(models.Model):
-#     # ResourceSkills fields here
-#     resource_skill = models.ForeignKey(ResourceSkills, on_delete=models.CASCADE)
-#     skill_level = models.ForeignKey(SkillLevel, on_delete=models.CASCADE)
-#     created_at = models.DateTimeField(auto_now_add=True, blank=True)
-#     updated_at = models.DateTimeField(auto_now=True, blank=True)
+class ResourceSkill(models.Model):
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+    level = models.FloatField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
